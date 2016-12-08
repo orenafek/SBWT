44a70bad9c5ad0cff5536e3e79083ee596a3603b
orimarco <marcovitch.ori@gmail.com>
2016-12-08 14:24:26 +0200

    Added Skelaton

--- SBWT/src/Main.scala
+++ SBWT/src/Main.scala
@@ -3,7 +3,34 @@
   */
 class Main {
 
+  type Suffix = Int
+
+  def Oren(word: List[Char]): List[Char] = {
+
+    val padded = padWord(word, '$')
+
+    val suffixes = wordSuffixes(padded)
+    val groups = splitToThreeGroups(suffixes)
+    val sortedFirstGroup = radixSort(groups._1)
+    val mergedOtherGroups = tripletsMergeSort(groups._2, groups._3)
+    val allMerged = merge(sortedFirstGroup, mergedOtherGroups)
+    extractLastColumn(allMerged)
+
+  }
+
+  def padWord(word: List[Char], char: Char): List[Char] = ???
+
+  def wordSuffixes(word: List[Char]): List[Suffix] = ???
+
+  def splitToThreeGroups(word: List[Suffix]): (List[Suffix], List[Suffix], List[Suffix]) = ???
+
   def merge() = ???
 
+  def radixSort(group: List[Suffix]): List[Suffix] = ???
 
+  def tripletsMergeSort(g1: List[Suffix], g2: List[Suffix]): List[Suffix] = ???
+
+  def merge(g1: List[Suffix], g2: List[Suffix]): List[Suffix] = ???
+
+  def extractLastColumn(indexes: List[Suffix]): List[Char] = ???
 }
