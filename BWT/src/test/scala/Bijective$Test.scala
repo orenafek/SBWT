d64da24eb873070902f0b8ebac6a14fd8a2e8f34
Oren <oren.afek@gmail.com>
2017-02-05 17:42:59 +0200

    Added few more tests

--- BWT/src/test/scala/Bijective$Test.scala
+++ BWT/src/test/scala/Bijective$Test.scala
@@ -15,7 +15,9 @@ class Bijective$Test extends FunSuite {
   }
 
   test("testFactorize") {
+    assertResult("ABC".toList :: Nil)(Bijective.factorize("ABC".toList))
     assertResult("OR".toList :: "EN".toList :: Nil)(Bijective.factorize("OREN".toList))
+    assertResult("OR".toList :: "E".toList :: "EN".toList :: Nil)(Bijective.factorize("OREEN".toList))
   }
 
 }
