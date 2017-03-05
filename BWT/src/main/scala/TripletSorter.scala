702310bcfab015261ffa043073ad029d04650f89
orimarco <marcovitch.ori@gmail.com>
2017-03-05 14:24:22 +0200

    OMG Merge

--- BWT/src/main/scala/TripletSorter.scala
+++ BWT/src/main/scala/TripletSorter.scala
@@ -9,9 +9,9 @@ import scala.runtime.Nothing$
   */
 object TripletSorter {
 
-  case class Triplet(idx: Int, ltrs: IntList) {}
+  case class Triplet(wordIdx: Int, ltrs: IntList) {}
 
-  case class IndexedTriplet(i: Int, override val idx: Int, override val ltrs: IntList) extends Triplet(idx, ltrs)
+  case class IndexedTriplet(i: Int, override val wordIdx: Int, override val ltrs: IntList) extends Triplet(wordIdx, ltrs)
 
   type IntList = List[Int]
   type IndexedIntList = List[Marco]
@@ -22,7 +22,7 @@ object TripletSorter {
   def mkGroups(src: IndexedIntList): (TripList, TripList) = {
     val $ = Range(0, src.size - 2)
       .map(i => IndexedTriplet(src(i).ord, i, List(src(i).tkn, src(i + 1).tkn, src(i + 2).tkn))).toList
-    def group0Cond(x: IndexedTriplet) = x.idx % 3 == 0
+    def group0Cond(x: IndexedTriplet) = x.wordIdx % 3 == 0
     ($.filter(group0Cond), $.filter(!group0Cond(_)))
   }
 
@@ -34,9 +34,50 @@ object TripletSorter {
   def sort(src: Word): Word = ???
 
   def merge(g0: TripList, g1_2: TripList): TripList = {
-    val t = Triplet(4, Nil)
 
-    ???
+    def merge_inner(g0: TripList, g1_2: TripList, suffixes: OrderedSuffixes): TripList = {
+      def proceedWithG0: TripList = g0.head :: merge_inner(g0.tail, g1_2, suffixes)
+      def proceedWithG12: TripList = g1_2.head :: merge_inner(g0, g1_2.tail, suffixes)
+      if (g0.isEmpty) g1_2
+      else if (g1_2.isEmpty) g0
+      else {
+        val i = g1_2.head.wordIdx
+        val j = g0.head.wordIdx
+        val S_i = g1_2.head.ltrs.head
+        val S_j = g0.head.ltrs.head
+        if (S_i < S_j)
+          proceedWithG12
+        else if (S_i > S_j)
+          proceedWithG0
+        else {
+          if (i % 3 == 1)
+          // group 1
+            if (suffixes.get(i + 1) < suffixes.get(j + 1))
+              proceedWithG12
+            else proceedWithG0
+          else { // group 2
+            val S_i_1 = g1_2.head.ltrs(1)
+            val S_j_1 = g0.head.ltrs(1)
+            if(S_i_1 < S_j_1)
+              proceedWithG12
+            else if (S_j_1 < S_i_1)
+              proceedWithG0
+            else if (suffixes.get(i + 2) < suffixes.get(j + 2))
+              proceedWithG12
+            else proceedWithG0
+
+          }
+
+
+
+        }
+
+        // take
+      }
+    }
+
+    merge_inner(g0, g1_2, new OrderedSuffixes(g1_2))
+
   }
 
   def radixSort(g0: TripList, handler: TripList): TripList = ???
@@ -73,6 +114,22 @@ object TripletSorter {
     radixSort_aux(src, 0)
   }
 
+  class OrderedSuffixes() {
+    val magicNo = 1.5
+    var data: Array[Int] = _
+
+    def this(lst: TripList) = this() {
+      val temp = lst.toArray
+      data = new Array(scala.math.ceil(magicNo * lst.size).toInt)
+      for (i <- temp.indices) {
+        data(temp(i).wordIdx) = i
+      }
+    }
+
+    def get(i: Int): Int = data(i)
+
+
+  }
 
 }
 
