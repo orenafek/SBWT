57c49cace6c96a04aded4e1367ba339b7d7c07d2
orimarco <marcovitch.ori@gmail.com>
2017-03-05 13:24:03 +0200

    go awesome

--- BWT/src/main/scala/TripletSorter.scala
+++ BWT/src/main/scala/TripletSorter.scala
@@ -8,31 +8,35 @@ import scala.runtime.Nothing$
   * @since 22/02/17
   */
 object TripletSorter {
+
+  case class Triplet(idx: Int, ltrs: IntList) {}
+
+  case class IndexedTriplet(i: Int, override val idx: Int, override val ltrs: IntList) extends Triplet(idx, ltrs)
+
   type IntList = List[Int]
-  type IndexedIntList = List[(Int, Int)]
-  type Triplet = (Int, IntList)
-  type IndexedTriplet = (Int, Triplet)
+  type IndexedIntList = List[Marco]
   type TripList = List[IndexedTriplet]
 
-  def mkWord(src: TripList): Word = src.flatMap(_._2._2.map(_.toChar))
+  def mkWord(src: TripList): Word = src.flatMap(_.ltrs.map(_.toChar))
 
   def mkGroups(src: IndexedIntList): (TripList, TripList) = {
     val $ = Range(0, src.size - 2)
-      .map(i => (src(i)._2, (i, List(src(i)._1, src(i + 1)._1, src(i + 2)._1)))).toList
-    def group0Cond(x: IndexedTriplet) = x._2._1 % 3 == 0
+      .map(i => IndexedTriplet(src(i).ord, i, List(src(i).tkn, src(i + 1).tkn, src(i + 2).tkn))).toList
+    def group0Cond(x: IndexedTriplet) = x.idx % 3 == 0
     ($.filter(group0Cond), $.filter(!group0Cond(_)))
   }
 
   def mkTriplets(src: IntList): TripList = mkTriplets_aux(src.map(_.toInt))
 
   def mkTriplets_aux(src: IntList): TripList = Range(0, src.size - 2)
-    .map(i => (-1, (i, List(src(i), src(i + 1), src(i + 2))))).toList
+    .map(i => IndexedTriplet(-1, i, List(src(i), src(i + 1), src(i + 2)))).toList
 
   def sort(src: Word): Word = ???
 
   def merge(g0: TripList, g1_2: TripList): TripList = {
+    val t = Triplet(4, Nil)
 
-
+    ???
   }
 
   def radixSort(g0: TripList, handler: TripList): TripList = ???
@@ -40,9 +44,9 @@ object TripletSorter {
   def sort_aux(src: IndexedIntList): TripList = {
     if (src.size == 1)
       ???
-//      List(src.head._2,(-1,???))
-//    else if (src.isEmpty)
-//      Nil
+    //      List(src.head._2,(-1,???))
+    //    else if (src.isEmpty)
+    //      Nil
     else {
       val (g0, g1_2) = mkGroups(src)
       val handler: TripList = oneTwoHandler(g1_2)
@@ -53,7 +57,7 @@ object TripletSorter {
   def oneTwoHandler(src: TripList): TripList = {
     val $ = radixSort(src)
     val o = new Ordinal
-    sort_aux($.map(o.next)).map(x => $(x._1))
+    sort_aux($.map(o.next)).map(x => $(x.i))
   }
 
 
@@ -64,7 +68,7 @@ object TripletSorter {
   def radixSort(src: TripList): TripList = {
     def radixSort_aux(src: TripList, i: Int): TripList = {
       if (i == 3) src
-      else src.groupBy(_._2._2(i)).map(x => (x._1, radixSort_aux(x._2, i + 1))).toList.sortBy(_._1).flatten(_._2)
+      else src.groupBy(_.ltrs(i)).map(x => (x._1, radixSort_aux(x._2, i + 1))).toList.sortBy(_._1).flatten(_._2)
     }
     radixSort_aux(src, 0)
   }
@@ -72,17 +76,19 @@ object TripletSorter {
 
 }
 
+case class Marco(tkn: Int, ord: Int) {}
+
 class Ordinal {
   var i: Int = -1
   var j: Int = -1
-  var last: IndexedTriplet = (-1, (-1, List(-1, -1, -1)))
+  var last: IndexedTriplet = IndexedTriplet(-1, -1, List(-1, -1, -1))
 
-  def next(current: IndexedTriplet): (Int, Int) = {
+  def next(current: IndexedTriplet): Marco = {
     if (last != current)
       i += 1
     j += 1
     last = current
-    (i, j)
+    Marco(i, j)
   }
 }
 
