9b9e9685810aa679db59164dc4b6c4f355a0f369
orimarco <marcovitch.ori@gmail.com>
2017-03-05 15:38:05 +0200

    תנאי עצירה

--- BWT/src/main/scala/TripletSorter.scala
+++ BWT/src/main/scala/TripletSorter.scala
@@ -1,35 +1,34 @@
 import Bijective.Word
-import TripletSorter.{IndexedTriplet, Triplet}
-
-import scala.runtime.Nothing$
+import TripletSorter.IndexedTriplet
 
 /**
   * @author Oren Afek
   * @since 22/02/17
   */
 object TripletSorter {
+  val ∞ = Int.MaxValue
 
   case class Triplet(wordIdx: Int, ltrs: IntList) {}
 
   case class IndexedTriplet(i: Int, override val wordIdx: Int, override val ltrs: IntList) extends Triplet(wordIdx, ltrs)
 
   type IntList = List[Int]
-  type IndexedIntList = List[Marco]
+  type IndexedIntWord = List[Marco]
   type TripList = List[IndexedTriplet]
 
   def mkWord(src: TripList): Word = src.flatMap(_.ltrs.map(_.toChar))
 
-  def mkGroups(src: IndexedIntList): (TripList, TripList) = {
+  def mkGroups(src: IndexedIntWord): (TripList, TripList) = {
     val $ = Range(0, src.size - 2)
       .map(i => IndexedTriplet(src(i).ord, i, List(src(i).tkn, src(i + 1).tkn, src(i + 2).tkn))).toList
     def group0Cond(x: IndexedTriplet) = x.wordIdx % 3 == 0
     ($.filter(group0Cond), $.filter(!group0Cond(_)))
   }
 
-  def mkTriplets(src: IntList): TripList = mkTriplets_aux(src.map(_.toInt))
+  def mkTriplets(src: IndexedIntWord): TripList = mkTriplets_aux(src)
 
-  def mkTriplets_aux(src: IntList): TripList = Range(0, src.size - 2)
-    .map(i => IndexedTriplet(-1, i, List(src(i), src(i + 1), src(i + 2)))).toList
+  def mkTriplets_aux(src: IndexedIntWord): TripList = Range(0, src.size - 2)
+    .map(i => IndexedTriplet(-1, i, List(src(i).tkn, src(i + 1).tkn, src(i + 2).tkn))).toList
 
   def sort(src: Word): Word = ???
 
@@ -55,39 +54,29 @@ object TripletSorter {
             if (suffixes.get(i + 1) < suffixes.get(j + 1))
               proceedWithG12
             else proceedWithG0
-          else { // group 2
+          else {
+            // group 2
             val S_i_1 = g1_2.head.ltrs(1)
             val S_j_1 = g0.head.ltrs(1)
-            if(S_i_1 < S_j_1)
+            if (S_i_1 < S_j_1)
               proceedWithG12
             else if (S_j_1 < S_i_1)
               proceedWithG0
             else if (suffixes.get(i + 2) < suffixes.get(j + 2))
               proceedWithG12
             else proceedWithG0
-
           }
-
-
-
         }
-
-        // take
       }
     }
-
     merge_inner(g0, g1_2, new OrderedSuffixes(g1_2))
-
   }
 
   def radixSort(g0: TripList, handler: TripList): TripList = ???
 
-  def sort_aux(src: IndexedIntList): TripList = {
-    if (src.size == 1)
-      ???
-    //      List(src.head._2,(-1,???))
-    //    else if (src.isEmpty)
-    //      Nil
+  def sort_aux(src: IndexedIntWord): TripList = {
+    if (src.size <= 3)
+      radixSort(mkTriplets(src ::: List(Marco(∞, -1), Marco(∞, -1))))
     else {
       val (g0, g1_2) = mkGroups(src)
       val handler: TripList = oneTwoHandler(g1_2)
@@ -98,7 +87,9 @@ object TripletSorter {
   def oneTwoHandler(src: TripList): TripList = {
     val $ = radixSort(src)
     val o = new Ordinal
-    sort_aux($.map(o.next)).map(x => $(x.i))
+    var li = List(Marco(∞, -1), Marco(∞, -1)) //two dollars for the end
+    $.foreach(x => li = o.next(x) :: li)
+    sort_aux(li).map(x => $(x.i))
   }
 
 
