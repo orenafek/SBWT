07be8c2cce58820a73e18e97e32df2d8d8555e12
orimarco <marcovitch.ori@gmail.com>
2017-03-25 23:15:59 +0300

    enter

--- BWT/src/test/scala/LyndonBijective$Test.scala
+++ BWT/src/test/scala/LyndonBijective$Test.scala
@@ -13,15 +13,15 @@ class LyndonBijective$Test extends FunSuite {
   private val LoremIpsum: String = "Lorem ipsum dolor sit amet, pri ad probo populo doctus, ei mel brute " +
     "dolores. Ei labore euismod qui. Ei natum appareat salutandi mea. Meliore dolores ex nam, mea elit " +
     "urbanitas ut, pro ea aliquip delectus repudiandae. Nec ei exerci graecis. No vis ullum iudico explicari," +
-    " nominavi torquatos vim no, et eam pertinax moderatius.\n\nVis id quot pertinacia. At omnis reprehendunt " +
+    " nominavi torquatos vim no, et eam pertinax moderatius.\nVis id quot pertinacia. At omnis reprehendunt " +
     "nec, ex debet nullam vix. Aliquid sanctus posidonium eum in, illum utinam at nec. Nam audiam volumus id, " +
-    "has ne legimus mediocritatem.\n\nVel et ponderum consulatu. Ad est principes moderatius, ea qui quis " +
+    "has ne legimus mediocritatem.\nVel et ponderum consulatu. Ad est principes moderatius, ea qui quis " +
     "cetero conclusionemque, ne has paulo soleat adolescens. Ut nam tale eruditi volutpat, mea ex stet " +
-    "voluptaria, democritum omittantur pro ex. Eos at adhuc appareat ullamcorper.\n\nQuo ne aeque epicuri " +
+    "voluptaria, democritum omittantur pro ex. Eos at adhuc appareat ullamcorper.\nQuo ne aeque epicuri " +
     "intellegam, dolore abhorreant sit ex. Est an iracundia reprehendunt. Sit viris legere numquam ad, " +
     "alii partem ex qui. Sed ea quem esse pertinax, no est posse liber mandamus, eum probo fugit no. " +
     "Qui ex vitae euismod reprehendunt, quot putant persius eam te. Dignissim dissentias his et, id usu " +
-    "probo zril.\n\nPer illum liber et. Facer vidisse ex vim. No postea suscipiantur duo. Porro tritani " +
+    "probo zril.\nPer illum liber et. Facer vidisse ex vim. No postea suscipiantur duo. Porro tritani " +
     "nam eu."
   private val BWT = new BurrowsWheelerTransform
   private val GST = GilScottBijectiveTransform
@@ -72,6 +72,9 @@ class LyndonBijective$Test extends FunSuite {
   }
 
   test("slower--linear") {
+    println(BWT.transformLinear(paper.toList).mkString)
+    println(BWT.transformSlow(paper.toList).mkString)
+
     assertResult(BWT.transformLinear(paper.toList).mkString)(BWT.transformSlow(paper.toList).mkString)
   }
   test("paperLinear") {
